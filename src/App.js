68a110ee7cfceab509a78384014327516d99305c
Patricia <cikunjoroge4@gmail.com>
2024-05-23 10:42:22 +0300

    Add consumed APIs component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Components/features/TestReducer';
+import ConsumeAPIs from './Components/features/axios/consumeAPIs';
 
 function App() {
   return (
     <div className="App">
      <Counter />
+     <ConsumeAPIs />
     </div>
   );
 }
