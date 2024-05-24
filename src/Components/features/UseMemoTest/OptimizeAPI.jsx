b0b281524c755eca34237c6679e199a7ec12981f
Patricia <cikunjoroge4@gmail.com>
2024-05-24 14:48:34 +0300

    feat: Optimize API calls using useMemo

---
+++ src/Components/features/UseMemoTest/OptimizeAPI.jsx
@@ -0,0 +1,8 @@
+// Best practices
+// Use State Management
+// Memoization
+// Debounce and throttle
+// Lazy loading
+// Pagination
+// Reduce Component Rerenders => React.memo, useMemo, PureComponent
+// 
--- src/Components/features/lazyLoading/LazyItem.jsx
+++ src/Components/features/lazyLoading/LazyItem.jsx
@@ -1,21 +1,26 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { getUsers } from "../axios/apis";
 const LazyItem = () => {
-      const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-      const fetchUsers = async () => {
-        try {
-          const data = await getUsers();
-          // console.log(data);
-          setUsers(data);
-        } catch (error) {
-          console.error("Error fetching user", error);
-        }
-      };
+  const fetchUsers = useMemo(
+    () => async () => {
+      try {
+        const data = await getUsers();
+        // console.log(data);
+        setUsers(data);
+      } catch (error) {
+        console.error("Error fetching user", error);
+      }
+    },
+    []
+  );
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
+  // console.log("Users", users);
 
-      fetchUsers();
-    }, []);
+  // console.log("Users",users)
   return <div>This is a lazy-loaded component!</div>;
 };
 export default LazyItem;
