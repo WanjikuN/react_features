349c0e126b5069ff72f2fc373d452dae78e4bc1e
Patricia <cikunjoroge4@gmail.com>
2024-05-31 08:32:43 +0300

    refactor: Update loaders and navigation

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import LazyLoadedComponents from './Components/features/lazyLoading/LazyLoading'
 import ShoppingCart from './Components/features/UseMemoTest/UseMemoTest';
 import UseRefHook from './Components/features/hooks/UseRefHook';
 import UseStateHook from './Components/features/hooks/UseStateHook';
+import Loader1 from './Components/Loaders/loader1';
 
 function App() {
   return (
     <div className="App">
      <Counter />
-     <ConsumeAPIs />
+     {/* <ConsumeAPIs /> */}
      <LazyLoadedComponents />
      <ShoppingCart />
-     <UseRefHook />
-     <UseStateHook />
+     {/* <UseRefHook /> */}
+     {/* <UseStateHook /> */}
+     <Loader1 />
     </div>
   );
 }
